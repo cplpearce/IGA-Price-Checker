366e93db78a7ecc1db50b252c5d6a07d958db5ae
Clinton Pearce <clint.pearce@rocketmail.com>
2020-06-09 23:20:39 -0400

    Complete change +improvements
    Had to do a rewrite due to IGA site changes.  Ref main readme

--- IGA_Scraper.py
+++ IGA_Scraper.py
@@ -1,16 +1,19 @@
 from colorama import Fore, Back, Style
 from bs4 import BeautifulSoup as bs
 from operator import itemgetter
+from selenium import webdriver
 import datetime
 import requests
-import colorama
+import time
 import math
+import sys
 import re
+import os
+
+# set local as dir
+os.chdir(sys.path[0])
 now = datetime.datetime.now()
 time_string = "{}-{}-{}".format(now.year, now.month, now.day)
-
-# init color console
-colorama.init()
 
 # # #   M A K E   C S V   # # #
 categoriesText = [
@@ -30,13 +33,14 @@ categoriesText = [
     "Refrigerated Grocery",
     "Seafood"
     ]
+
 categoriesUrlsExtensions = [
     r"in-promotion",
     r"Beverages/in-promotion",
     r"Bulk%20Foods/in-promotion",
     r"Commercial%20Bakery/in-promotion",
     r"Deli%20and%20Cheese/in-promotion",
-    r"Frozen/in-promotion", 
+    r"Frozen/in-promotion",
     r"Grocery/in-promotion",
     r"Health%20%26%20Beauty/in-promotion",
     r"Health%20Care/in-promotion",
@@ -48,26 +52,32 @@ categoriesUrlsExtensions = [
     r"Seafood/in-promotion"
     ]
 
-print("Please enter desired category number: ")
-i = 1
-for cat in categoriesText:
-    print("{}: {}".format(i, cat))
-    i += 1
+def clean(istr):
+    if "\n" in istr:
+        istr = istr.replace(r"\n", "")
+    if "$" in istr:
+        istr = istr.replace("$", "")
+    return(istr)
 
-userCategory = int(input("\n"))
+print("Please enter desired category number: ")
+for i, cat in enumerate(categoriesText, 1):
+    print("{}: {}".format(i, cat))
+
+userCategory = int(input("Choose your category: "))
 1 if userCategory not in range(1,16) else int(userCategory)
 # down one to account for lists
 userCategory -= 1
 
 cv_name = "igaSalesExport {}-{}.csv".format(categoriesText[userCategory], time_string)
 with open(cv_name, "w") as csv:
-    
+
     csv.write("Item Name,Item Category/Brand,Regular Price,Sale Price,Total Sale Discount,Discount Ratio")
 
 # # #   G E T   P A G E S   # # #
+# init driver
+driver = webdriver.Firefox(executable_path=r"...\geckodriver.exe")
 
 urlDefaultLanding = r"https://www.iga.net/en/online_grocery/browse/{}".format(categoriesUrlsExtensions[userCategory])
-
 pagesScrape = requests.get(urlDefaultLanding)
 pagesHtmlDocument = pagesScrape.content
 soup = bs(pagesHtmlDocument, "html.parser")
@@ -75,57 +85,68 @@ soup = bs(pagesHtmlDocument, "html.parser")
 # # #   C R E A T E   U R L S   # # #
 
 urls = []
-for p in range(1,10):
-  urls.append("https://www.iga.net/en/online_grocery/browse/{}?pageSize=1000&page={}".format(categoriesUrlsExtensions[userCategory], p))
+for p in range(1,20):
+  urls.append("https://www.iga.net/en/online_grocery/browse/{}?pageSize=200&page={}".format(categoriesUrlsExtensions[userCategory], p))
 
 # # #   P A R S E   U R L S   # # #
 
-print("Working on URL:")
-
 itemCatalog = []
 
-for url in urls:        
-    productsScrape = requests.get(url)
-    htmlDocument = productsScrape.content
-    soup = bs(htmlDocument, "html.parser")
+driver.get("https://www.iga.net/en/online_grocery/browse/in-promotion")
 
+for url in urls:
+
+    print(f"Parsing {url}")
+    driver.get(url)
+
+    htmlDocument = driver.page_source
+
+    soup = bs(htmlDocument, "html.parser")
+    counter = 0
+    found = 0
     for productGridItem in soup.find_all("div", "item-product__content push--top"):
+        found += 1
+        ### P R I C E ###
         try:
-            productSalePrice = productGridItem.find("span", "price text--strong")
-            productSalePrice = productSalePrice.text[1:]
-            productPrice = productGridItem.find("span", "price-amount")
-            productPrice = productPrice.text[1:]
-            productCategory = productGridItem.find("div", "item-product__brand push--top")
-            productCategory.text if productCategory.text == None else "Misc"
-            if productCategory.text != None:  productCategory = productCategory.text.strip()
-            else:
-                productCategory = "Misc"
-            productName = productGridItem.find("a", "js-ga-productname")
-            if productName.text[0] == " ": 
-                productName = productName.text[1:]
-            else:
-                productName = productName.text
-            productDiscount = "{:2.2f}".format(float(productPrice) - float(productSalePrice))
-            productSaleRatio = float(productDiscount) / float(productPrice)
-            productSaleRatio = "{:1.4f}".format(productSaleRatio)
-            productMetadataGroup = [
-                productName.replace(",","-"),
-                productCategory.replace(",","-"),
-                productPrice,
-                productSalePrice,
-                productDiscount,
-                productSaleRatio
-            ]
-            itemCatalog.append(productMetadataGroup)
+            productSalePrice = clean(productGridItem.find("span", {"class": "price text--strong"}).text)
+            productPrice = clean(productGridItem.find("span", "price-amount").text)
         except AttributeError:
-            pass
-            
+            continue
+        ### D E T A I L S ###
+        try: 
+            productCategory = productGridItem.find("div", "item-product__brand push--top").text
+            productCategory = [re.findall(r'[^\S][A-Za-z]+', productCategory, flags=re.DOTALL)]
+            productCategory= productCategory.join("", productCategory)
+        except AttributeError:
+            productCategory = "Misc"
+        productName = productGridItem.find("a", "js-ga-productname")
+        if productName.text[0] == " ":
+            productName = productName.text[1:]
+        else:
+            productName = productName.text
+        ### M A T H ###
+        productDiscount = "{:2.2f}".format(float(productPrice) - float(productSalePrice))
+        productSaleRatio = float(productDiscount) / float(productPrice)
+        productSaleRatio = "{0:.0%}".format(productSaleRatio)
+        productMetadataGroup = [
+            productName.replace(",","-"),
+            productCategory.replace(",","-"),
+            productPrice,
+            productSalePrice,
+            productDiscount,
+            productSaleRatio
+        ]
+        itemCatalog.append(productMetadataGroup)
+        counter += 1
+        print(productMetadataGroup)
+        print([found, counter])
+
 itemCatalog = sorted(itemCatalog, key = itemgetter(5), reverse=True)
-    
+
 # # #   W R I T E   T O   C S V   # # #
 
 with open(cv_name, "a") as csv:
-                
+
     for product in itemCatalog:
         csv.write(
         "\n{},{},${},${},${},{}".format(
@@ -137,22 +158,5 @@ with open(cv_name, "a") as csv:
             product[5]
             )
         )
-"""        
-for product in itemCatalog:
-    print("{}Item Name: {}{}\n{}Item Category/Brand: {}{}\nRegular Price: ${}\nSale Price: ${}\n{}Total Sale Discount: ${}{}\n".format(
-        Fore.CYAN,
-        product[0],
-        Style.RESET_ALL,
-        Fore.GREEN,
-        product[1],
-        Style.RESET_ALL,
-        product[2],
-        product[3],
-        Fore.RED,
-        product[4],
-        Style.RESET_ALL
-        )
-    )
-"""
-    
+
 print("Finished!")
